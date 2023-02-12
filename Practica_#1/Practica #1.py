78b584c8cc5d31833e4d6a7d67374c52a6acfcb4
Edwin Andres <70546772+Edwin-Andres222@users.noreply.github.com>
2023-02-12 16:36:57 -0600

    Subir-Practica_1
    practica 1 - Edwin

---
+++ Practica_#1/Practica #1.py
@@ -0,0 +1,11 @@
+
+#FUNCION CON N PARAMETROS
+
+#Escribir un programa que contenga una función que reciba n parámetros de tipo numérico y calcule el 
+#producto total
+
+def Calculos(a,b,c):
+    total = (a+b)*c
+    return total
+
+print("El total de (a+b)*c = ",Calculos(10,10,2))
