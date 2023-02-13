86345f47c293c1a07d0f983864b582e869f2b421
JesusGuMe <gutierrezmjesus6@gmail.com>
2023-02-13 06:57:57 -0600

    Se añade la solución a la practica 3.

---
+++ Practica_#3/Practica3.py
@@ -0,0 +1,6 @@
+#3 Entrada de datos y manipulación.
+# Escribir un programa que permita al usuario capturar su nombre completo e imprima su nombre de 
+# manera inversa letra por letra
+
+nombre = input("Introducir nombre: ")
+print(nombre[::-1])
