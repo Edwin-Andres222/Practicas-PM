24aaec7e654d54218295a750b4e565d2fb0ce158
Edwin Andres <70546772+Edwin-Andres222@users.noreply.github.com>
2023-04-14 22:46:30 -0500

    Actividades
    subir actividades

---
+++ Practica_#1_Unidad_#2/commits.py
@@ -0,0 +1,49 @@
+import psycopg2
+from logger_base import log
+conexion = psycopg2.connect(user="postgres"
+                            ,password="AZXW157e"
+                            ,host="LOCALHOST"
+                            ,port="5432"
+                            ,database="Practicadb")
+print(conexion)
+
+try:
+    conexion.autocommit=False
+    cursor = conexion.cursor()
+    sentencia = "INSERT INTO cliente(nombre,apellido,email) Values(%s,%s,%s)"
+    nombre ='Juan'
+    valores = (nombre,'Perez','jperez@gmail.com')
+    
+    cursor.execute(sentencia,valores)
+    log.debug("Sentencia Insert")
+    sentencia = "UPDATE cliente SET nombre=%s , apellido=%s, email=%s WHERE id_usuario=%s"
+    valores=("Hugo","sanchez","hsanchez@mail.com",1)
+    cursor.execute(sentencia,valores)
+    log.debug("Sentencia Update")
+    conexion.commit()
+    log.debug("Cambios guardados")
+except Exception as e:
+    conexion.rollback()
+    log.error(e)
+
+
+try:
+    conexion.autocommit=False
+    cursor = conexion.cursor()
+    sentencia = "INSERT INTO cliente(nombre,apellido,email) Values(%s,%s,%s)"
+    nombre ='Juan'
+    valores = (nombre,'Perez','jperez@gmail.com')
+    
+    cursor.execute(sentencia,valores)
+    log.debug("Sentencia Insert")
+    sentencia = "UPDATE cliente SET nombre=%s , apellido=%s, email=%s WHERE id_usuario=%s"
+    valores=("Hugo","sanchez","hsanchez@mail.com",1)
+    cursor.execute(sentencia,valores)
+    log.debug("Sentencia Update")
+    conexion.commit()
+    log.debug("Cambios guardados")
+except Exception as e:
+    conexion.rollback()
+    log.error(e)
+
+
